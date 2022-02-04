81c804fa48ecd12bf875d465daa3ec5ff543fe2a
FreedomWriter <natalienetworks@gmail.com>
2022-02-04 14:48:31 -0600

    config

--- next.config.js
+++ next.config.js
@@ -6,15 +6,15 @@ module.exports = {
   ) {
     return {
       "/": { page: "/" },
-      "blog/posts/test-post": {
+      "/blog/posts/test-post": {
         page: "blog/posts/[slug]",
         query: { slug: "test-post" },
       },
-      "blog/posts/test-post-not-featured": {
+      "/blog/posts/test-post-not-featured": {
         page: "bolg/posts/[slug]",
         query: { slug: "test-post-not-featured" },
       },
-      "blog/posts/second-test-post-not-featured": {
+      "/blog/posts/second-test-post-not-featured": {
         page: "bolg/posts/[slug]",
         query: { slug: "second-test-post-not-featured" },
       },
