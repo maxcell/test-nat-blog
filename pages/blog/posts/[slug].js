6c43ee693d43507afa67b067390e05242f06d186
FreedomWriter <natalienetworks@gmail.com>
2022-02-04 14:27:18 -0600

    pulling at straws

--- pages/blog/posts/[slug].js
+++ pages/blog/posts/[slug].js
@@ -4,30 +4,31 @@ import { getPostData, getPostsFiles } from "../../../lib/post-utils";
 export default function PostDetailPage(props) {
   const { post } = props;
 
-  return <Post post={post} />;
+  // return <Post post={post} />;
+  return <h1>Yooooooo</h1>;
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
-  const { slug } = params;
-  const post = await getPostData(slug);
+// export async function getStaticProps(context) {
+//   const { params } = context;
+//   const { slug } = params;
+//   const post = await getPostData(slug);
 
-  return {
-    props: {
-      post,
-    },
-    revalidate: 600,
-  };
-}
+//   return {
+//     props: {
+//       post,
+//     },
+//     revalidate: 600,
+//   };
+// }
 
-export function getStaticPaths() {
-  const postFilenames = getPostsFiles();
-  console.log({ postFilenames });
+// export function getStaticPaths() {
+//   const postFilenames = getPostsFiles();
+//   console.log({ postFilenames });
 
-  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
-  console.log({ slugs });
-  return {
-    paths: slugs.map((slug) => ({ params: { slug } })),
-    fallback: "blocking",
-  };
-}
+//   const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+//   console.log({ slugs });
+//   return {
+//     paths: slugs.map((slug) => ({ params: { slug } })),
+//     fallback: false,
+//   };
+// }
