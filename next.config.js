34856b105390f05947297ca7ea176ecc600856a2
FreedomWriter <natalienetworks@gmail.com>
2022-02-04 14:52:23 -0600

    reverting next.config change

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 module.exports = {
   reactStrictMode: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-      "/blog/posts/test-post": {
-        page: "blog/posts/[slug]",
-        query: { slug: "test-post" },
-      },
-      "/blog/posts/test-post-not-featured": {
-        page: "bolg/posts/[slug]",
-        query: { slug: "test-post-not-featured" },
-      },
-      "/blog/posts/second-test-post-not-featured": {
-        page: "bolg/posts/[slug]",
-        query: { slug: "second-test-post-not-featured" },
-      },
-    };
-  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/blog/posts/test-post": {
+  //       page: "blog/posts/[slug]",
+  //       query: { slug: "test-post" },
+  //     },
+  //     "/blog/posts/test-post-not-featured": {
+  //       page: "bolg/posts/[slug]",
+  //       query: { slug: "test-post-not-featured" },
+  //     },
+  //     "/blog/posts/second-test-post-not-featured": {
+  //       page: "bolg/posts/[slug]",
+  //       query: { slug: "second-test-post-not-featured" },
+  //     },
+  //   };
+  // },
 };
