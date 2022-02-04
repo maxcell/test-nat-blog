6e23a9d008538d49fc72d98728b785851cd36f90
FreedomWriter <natalienetworks@gmail.com>
2022-02-04 16:01:29 -0600

    hardcoded pages

---
+++ pages/blog/posts/test-post-not-featured.js
@@ -0,0 +1,22 @@
+import { Post } from "components";
+import { getPostData } from "../../../lib/post-utils";
+
+export default function SelectedPost(props) {
+  const { post } = props;
+  return (
+    <div>
+      <Post post={post} />
+    </div>
+  );
+}
+export async function getStaticProps(context) {
+  console.log({ context });
+  const post = await getPostData("test-post-not-featured");
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 600,
+  };
+}
