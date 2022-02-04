891b0583d0aa2f03375f94d6b418634bc9655163
FreedomWriter <natalienetworks@gmail.com>
2022-02-04 16:08:07 -0600

    brought back dynamic route

--- components/Posts/PostBlurb/index.js
+++ components/Posts/PostBlurb/index.js
@@ -8,7 +8,6 @@ import {
   ContentContainer,
   H2,
   LI,
-  P,
   Time,
 } from "./styled";
 
---
+++ pages/blog/posts/[slug].js
@@ -0,0 +1,33 @@
+import { Post } from "components";
+import { getPostData, getPostsFiles } from "../../../lib/post-utils";
+
+export default function PostDetailPage(props) {
+  const { post } = props;
+
+  return <Post post={post} />;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const post = await getPostData(slug);
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+  console.log({ postFilenames });
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+  console.log({ slugs });
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
+}
