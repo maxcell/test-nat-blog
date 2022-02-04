6772a7714c9efde5183a897cc620883c050ea1af
FreedomWriter <natalienetworks@gmail.com>
2022-02-04 14:23:56 -0600

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   reactStrictMode: true,
-}
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/posts/test-post": {
+        page: "blog/posts/[slug]",
+        query: { slug: "test-post" },
+      },
+      "/posts/test-post-not-featured": {
+        page: "bolg/posts/[slug]",
+        query: { slug: "test-post-not-featured" },
+      },
+      "/posts/second-test-post-not-featured": {
+        page: "bolg/posts/[slug]",
+        query: { slug: "second-test-post-not-featured" },
+      },
+    };
+  },
+};
