f9e18f81f2f85a0d509177139d97f07f1da493e7
Prince Wilson <maxcell.wilson@gmail.com>
2022-02-04 19:06:19 -0500

    pls

--- pages/blog/posts/[slug].js
+++ pages/blog/posts/[slug].js
@@ -9,23 +9,23 @@ export default function PostDetailPage(props) {
     return <p>Loading</p>;
   }
 
-  return <p>yo</p>;
+  return <Post post={post} />
 }
 
 export async function getStaticProps(context) {
   const { params } = context;
   const { slug } = params;
-  const post = await getPostData(slug);
+  const post = getPostData(slug);
 
+  console.log({ onPage: post })
   return {
     props: {
       post,
     },
-    revalidate: 600,
   };
 }
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
   const postFilenames = getPostsFiles();
 
   const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
